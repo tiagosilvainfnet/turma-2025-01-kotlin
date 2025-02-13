0da49ded7c4af6c2d210058de5399fc8cff2d218
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-02-12 22:04:37 -0300

    Second commit profile screen

--- app/src/main/java/dev/tiagosilva/taskmanager/ProfileActivity.kt
+++ app/src/main/java/dev/tiagosilva/taskmanager/ProfileActivity.kt
@@ -1,14 +1,28 @@
 package dev.tiagosilva.taskmanager
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.tiagosilva.taskmanager.utils.Navigation
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val homeBtn = findViewById<ImageView>(R.id.home)
+        val logoutBtn = findViewById<ImageView>(R.id.logout)
+
+        homeBtn.setOnClickListener {
+            Navigation.goToScreen(this, MainActivity::class.java)
+        }
+
+        logoutBtn.setOnClickListener {
+            Navigation.goToScreen(this, LoginActivity::class.java)
+        }
+
     }
 }
