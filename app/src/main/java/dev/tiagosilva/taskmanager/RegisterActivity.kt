a36981a8aee7e684c75d3c7e2fdac1d9d4f767cb
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-02-19 22:06:20 -0300

    - Start firebase database - Fragment password

--- app/src/main/java/dev/tiagosilva/taskmanager/RegisterActivity.kt
+++ app/src/main/java/dev/tiagosilva/taskmanager/RegisterActivity.kt
@@ -1,12 +1,17 @@
 package dev.tiagosilva.taskmanager
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import dev.tiagosilva.taskmanager.fragments.PasswordDifficult
 import dev.tiagosilva.taskmanager.utils.Navigation
+import dev.tiagosilva.taskmanager.utils.Password
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -18,19 +23,44 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        lateinit var text: Editable;
 
         val btnRegister = findViewById<Button>(R.id.submitRegisterBtn)
         val forgotPassword = findViewById<TextView>(R.id.forgotPassword)
         val LoginLink = findViewById<TextView>(R.id.login)
+        val confirmPassword = findViewById<EditText>(R.id.passwordConfirmInput)
+        val password = supportFragmentManager.findFragmentById(R.id.passwordInput) as PasswordDifficult
 
         btnRegister.setOnClickListener {
             val email = findViewById<TextView>(R.id.emailInput).text.toString()
-            val password = findViewById<TextView>(R.id.passwordInput).text.toString()
 
             CoroutineScope(Dispatchers.IO).launch {
-                register(email, password)
+                val passText = password.passwordInput.text.toString()
+                val confirmPasswordText = confirmPassword.text.toString()
+
+                register(email, passText, confirmPasswordText)
             }
         }
+
+        password.passwordInput.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s != null) {
+                    text = s;
+                }
+                val result = Password.verifyPasswordDificult(text.toString());
+                if (result < 3) {
+                    btnRegister.isEnabled = false;
+                } else {
+                    btnRegister.isEnabled = true;
+                }
+            }
+
+        })
+
 
         forgotPassword.setOnClickListener {
             Navigation.goToScreen(this, ForgotPasswordActivity::class.java)
@@ -41,23 +71,37 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    fun register(email: String, password: String) {
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        baseContext,
-                        "Usuário cadastrado com sucesso!!!",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    Navigation.goToScreen(this, MainActivity::class.java)
-                } else {
-                    Toast.makeText(
-                        baseContext,
-                        "Falha ao registrar usuário.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
+    fun register(email: String, password: String, confirmPassword: String) {
+        if (password != confirmPassword){
+            Toast.makeText(
+                baseContext,
+                "As senhas não são iguais",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }else if (password == "" || confirmPassword == ""){
+            Toast.makeText(
+                baseContext,
+                "As senhas não podem ser vazias",
+                Toast.LENGTH_SHORT,
+            ).show()
+        } else {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext,
+                            "Usuário cadastrado com sucesso!!!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        Navigation.goToScreen(this, MainActivity::class.java)
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Falha ao registrar usuário.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
                 }
-            }
+        }
     }
 }
